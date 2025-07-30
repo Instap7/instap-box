ba91a29cd6f7c8ccc217970c6c2c3aa7ca1d5186
Pawel <rzepaw@gmail.com>
2025-07-30 22:53:00 +0200

    Fix

--- instap/modbus_register.py
+++ instap/modbus_register.py
@@ -21,20 +21,16 @@ class ModbusRegister:
         self.created_at = self.data.get('created_at')
         self.tags = self.data.get('tags')
         self.type = self.data.get('type')
-        self.label = self.data.get('label')
-        self.device_model = self.data.get('device_model')
-        self.register_address = self.data.get('register_address')
-        self.register_type = self.data.get('register_type')
-        self.data_type = self.data.get('data_type')
-        self.scale_factor = self.data.get('scale_factor')
         self.unit = self.data.get('unit')
-        self.description = self.data.get('description')
-        self.min_value = self.data.get('min_value')
-        self.max_value = self.data.get('max_value')
-        self.default_value = self.data.get('default_value')
-        self.read_only = self.data.get('read_only')
-        self.write_only = self.data.get('write_only')
+        self.gain = self.data.get('gain')
+        self.offset = self.data.get('offset')
+        self.quantity = self.data.get('quantity')
+        self.address = self.data.get('address')
+        self.device_model = self.data.get('device_model')
+        self.parameter = self.data.get('parameter')
+        self.function = self.data.get('function')
+        self.label = self.data.get('label')
     
     def __str__(self):
         """String representation of the ModbusRegister."""
-        return f"ModbusRegister(slug='{self.slug}', name='{self.name}', register_address='{self.register_address}', register_type='{self.register_type}', data_type='{self.data_type}')" 
+        return f"ModbusRegister(slug='{self.slug}', name='{self.name}', address='{self.address}', function='{self.function}', type='{self.type}')" 
